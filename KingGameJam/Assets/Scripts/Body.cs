b640c887e349dcd459855d14eb9ab8230161be00
Alexander Lövberg <lovbergalexander@gmail.com>
2018-06-09 17:17:02 +0200

    Backup

--- KingGameJam/Assets/Scripts/Body.cs
+++ KingGameJam/Assets/Scripts/Body.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Body : MonoBehaviour {
 
+	public static Body Instance;
+
 	public GameObject head;
 	public Transform BodyParent;
 	public GameObject tail;
 
 	public GameObject bodyPrefab;
 
-	public float segmentLength;
+	public float segmentDistance = 1.5f;
+	public float scalePerSegment = .1f;
 
-	List<BodySegment> segments = new List<BodySegment>();
-	List<GameObject> spawnedSegments = new List<GameObject>();
+	[HideInInspector]
+	public float currentScale = 1;
+
+	public List<BodySegment> segments = new List<BodySegment>();
+	public List<GameObject> spawnedSegments = new List<GameObject>();
 
 	void Start ()
 	{
+		currentScale = 1;
 		BodySegment head = new BodySegment();
 		head.gameObject = this.head;
 		segments.Add(head);
@@ -24,21 +31,40 @@ public class Body : MonoBehaviour {
 		AddSegment();
 		AddSegment();
 		AddSegment();
-		AddSegment();		
+		AddSegment();
 	}
 
 	public void AddSegment ()
 	{
 		BodySegment segment = new BodySegment();
 		segment.parent = segments[segments.Count - 1];
-		segment.gameObject = Instantiate(bodyPrefab, transform.position, Quaternion.identity);
+		segment.gameObject = Instantiate(bodyPrefab, segment.parent.position - (Vector2)segment.parent.gameObject.transform.up, segment.parent.gameObject.transform.rotation);
 
-		segments.Add(segment);		
+		segments.Add(segment);
+
+		currentScale += scalePerSegment;
+		ChangeScale(currentScale);
 	}
 
 	public void RemoveSegment ()
 	{
-		segments.RemoveAt(segments.Count);
+		if(segments.Count <= 1)
+		{
+			return;
+		}
+
+		Destroy(segments[segments.Count - 1].gameObject);
+		segments.RemoveAt(segments.Count - 1);
+		currentScale -= scalePerSegment;
+		ChangeScale(currentScale);		
+	}
+
+	public void ChangeScale (float value)
+	{		
+		foreach (var segment in segments)
+		{
+			segment.gameObject.transform.localScale = Vector3.one * value;
+		}
 	}
 
 	public void UpdateSegments ()
@@ -51,27 +77,33 @@ public class Body : MonoBehaviour {
 			{
 				continue;
 			}
+
+			segment.gameObject.transform.up = segment.parent.position - segment.position;
 			
-			if(Vector3.Distance(segment.position, segment.parent.position) > segmentLength)
+			if(Vector3.Distance(segment.position, segment.parent.position) > segmentDistance)
 			{
 				MoveSegment(segment);
-				LineRenderer line = segment.gameObject.GetComponent<LineRenderer>();
-				line.SetPosition(0, segment.position);
-				line.SetPosition(1, (segment.parent.position));
 			}
 		}
 	}
 
 	void MoveSegment (BodySegment segment){
-		// Vector3.SmoothDamp(segment.position, )
-		// segment.gameObject.transform.Translate(segment.parent.position - segment.position);
-		// segment.gameObject.GetComponent<Rigidbody2D>().AddForce((segment.parent.position - segment.position) * 3);
 		Vector2 newPos = Vector2.SmoothDamp(segment.position, segment.parent.position, ref segment.velocity, .5f);
 		segment.position = newPos;
 	}
 
 	void Update () 
 	{
+		if(Input.GetKeyDown(KeyCode.KeypadPlus))
+		{
+			AddSegment();
+		}
+
+		if(Input.GetKeyDown(KeyCode.KeypadMinus))
+		{
+			RemoveSegment();
+		}
+
 		UpdateSegments();
 	}
 }
--- KingGameJam/Assets/Scripts/CameraController.cs
+++ KingGameJam/Assets/Scripts/CameraController.cs
@@ -6,19 +6,44 @@ public class CameraController : MonoBehaviour {
 
 	public Transform target;
 	[Range(0, 1)] public float smoothing;
+	Kraken kraken;
 
 	Vector3 velocity;
+	float originalZ;
+	Camera camera;
 
 	void Start () 
 	{
-		
+		kraken = Kraken.Instance;
+		originalZ = Camera.main.orthographicSize;
+		camera = Camera.main;
 	}
 	
 	void Update () 
 	{
+		Body kraken = this.kraken.GetComponent<Body>();
+
+		Bounds bounds = GetBounds(kraken);
+
+		// Vector3 zTarget = 
+
 		Vector3 newPos = Vector3.SmoothDamp(transform.position, target.transform.position, ref velocity, smoothing);
+
+		camera.orthographicSize = originalZ + kraken.currentScale * kraken.scalePerSegment;
+
 		newPos.z = transform.position.z;
 
 		transform.position = newPos;
+	}
+
+	Bounds GetBounds (Body body)
+	{
+		Bounds krakenBounds = new Bounds();
+		List<Vector2> positions = new List<Vector2>();
+
+		body.segments.ForEach(segment => positions.Add(segment.position));
+		positions.ForEach(pos => krakenBounds.Encapsulate(pos));
+		
+		return krakenBounds;
 	}
 }
