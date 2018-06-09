6e077dabcf064773860fbedb0aa1f8054dbfe3e4
Alexander Lövberg <lovbergalexander@gmail.com>
2018-06-09 14:53:11 +0200

    Moving monster

---
+++ KingGameJam/Assets/RotateTowardsVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsVelocity : MonoBehaviour {
+
+	Rigidbody2D rigidbody;
+
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		transform.up = rigidbody.velocity;
+	}
+}
