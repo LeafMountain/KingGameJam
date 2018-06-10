8b861487b55b12b8b20c1d8a656d17988f5f0829
Alexander Lövberg <lovbergalexander@gmail.com>
2018-06-10 13:57:25 +0200

    Kill stuff

---
+++ KingGameJam/Assets/Instantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour {
+	public GameObject go;
+	public int number = 1;
+
+	public void Trigger ()
+	{
+		for (int i = 0; i < number; i++)
+		{
+			Instantiate(go, transform.position, Quaternion.identity);
+			
+		}
+	}
+}
