7efe905b2e9f46cc6a142330623039e5645c71e2
axel stenkrona <axelstenkrona@me.com>
2018-06-10 12:37:40 +0200

    spawnFloaters

---
+++ KingGameJam/Assets/Scripts/Enemies/EnemyBaseScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseScript : MonoBehaviour {
+
+
+	public bool debugMode;
+
+	public GameObject floater;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+		DestroyTest();
+	}
+	private void DestroyTest(){
+		if(debugMode && Input.GetKeyUp(KeyCode.Space))
+		{
+			new SpawnFloaters(gameObject, floater);
+			Destroy(gameObject);
+		}
+	}
+	
+
+}
