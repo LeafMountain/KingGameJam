5142bf1885045c1b2c0861e339bb2a928807515a
Stenkrona <axelstenkrona@me.com>
2019-10-01 14:38:59 +0200

    buildsettings and controller scheme

---
+++ Assets/Code/UI/ReloadLevelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadLevelButton : MonoBehaviour
+{
+    [ExecuteInEditMode]
+   public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
